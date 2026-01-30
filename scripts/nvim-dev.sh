697c01bdd2f593cf09ecd8292c1e228b5c71db6e
uxnknwn <uxnkn_wn@proton.me>
2026-01-30 03:10:23 -0500

    Initial commit

---
+++ scripts/nvim-dev.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+# If network isn't compiled, exit
+if [ ! -d "network" ]; then
+    echo "Network folder missing. Exiting."
+    exit 1
+fi
+
+# If Packages aren't installed, exit
+if [ ! -d "Packages" ]; then
+    echo "Packages folder missing. Exiting."
+    exit 1
+fi
+
+
+rojo sourcemap default.project.json -o sourcemap.json
+
+rojo serve default.project.json
